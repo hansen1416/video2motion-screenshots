186a3449b897f4f7450857d0fc537db89b02ae85
hansen1416 <badapplesweetie@gmail.com>
2024-03-10 22:10:18 +0800

    1

--- anim-player/src/routes/GlbEuler.svelte
+++ anim-player/src/routes/GlbEuler.svelte
@@ -19,8 +19,13 @@
 	let anim_step = 0;
 	let max_anim_step = 0;
 
+	let animation_path = `/anim-json-euler/Stand Cover To Look.json`;
+	// let animation_path = `/anim-json-euler-pred/pred_Stand Cover To Look.json`;
+	let frame_skip = 1;
+	let counter = 0;
+
 	function animate() {
-		if (animation_data && bones) {
+		if (counter % frame_skip === 0 && animation_data && bones) {
 			for (let bone_name in bones) {
 				if (animation_data[bone_name] !== undefined) {
 					if (animation_data[bone_name][anim_step] === undefined) {
@@ -34,21 +39,28 @@
 					bones[bone_name].rotation.set(x, y, z);
 				}
 			}
-		}
 
-		anim_step += 1;
+			anim_step += 1;
 
-		if (anim_step > max_anim_step) {
-			anim_step = 0;
+			if (anim_step > max_anim_step) {
+				anim_step = 0;
+			}
 		}
 
 		// update physics world and threejs renderer
 		threeScene.onFrameUpdate();
 
+		counter += 1;
+
 		animation_pointer = requestAnimationFrame(animate);
 	}
 
 	onMount(() => {
+		// if `animation_path` start with /anim-json-euler-pred
+		if (animation_path.startsWith("/anim-json-euler-pred")) {
+			frame_skip = 3;
+		}
+
 		threeScene = new ThreeScene(
 			canvas,
 			document.documentElement.clientWidth,
@@ -58,63 +70,61 @@
 		// -100 is ground level
 		threeScene.scene.position.set(0, -1, 0);
 
-		Promise.all([
-			loadGLTF(`/glb/dors.glb`),
-			// loadJSON(`/anim-json-euler/Crouch Turn Left 90.json`),
-			loadJSON(`/anim-json-euler-pred/pred_Crouch Turn Left 90.json`),
-		]).then(([glb_model, anim_data]) => {
-			glb_model = glb_model.scene.children[0];
+		Promise.all([loadGLTF(`/glb/dors.glb`), loadJSON(animation_path)]).then(
+			([glb_model, anim_data]) => {
+				glb_model = glb_model.scene.children[0];
 
-			glb_model.name = "diva";
+				glb_model.name = "diva";
 
-			// console.log(fbx_unity_anim);
-			threeScene.scene.add(glb_model);
+				// console.log(fbx_unity_anim);
+				threeScene.scene.add(glb_model);
 
-			glb_model.traverse((node) => {
-				// @ts-ignore
-				if (node.isMesh) {
-					node.castShadow = true;
-				}
-				// @ts-ignore
-				if (node.isBone) {
+				glb_model.traverse((node) => {
 					// @ts-ignore
-					if (bones[node.name] === undefined) {
-						// somehow maximo has double bones, so only use the first one
-						bones[node.name] = node;
+					if (node.isMesh) {
+						node.castShadow = true;
+					}
+					// @ts-ignore
+					if (node.isBone) {
+						// @ts-ignore
+						if (bones[node.name] === undefined) {
+							// somehow maximo has double bones, so only use the first one
+							bones[node.name] = node;
+						}
+					}
+				});
+
+				console.log(bones);
+
+				// animation_data = {};
+
+				max_anim_step = anim_data["Hips"].values.length;
+
+				// animation_data = anim_data;
+				// iterate over anim_data object
+
+				animation_data = {};
+
+				for (let bone_name in anim_data) {
+					animation_data[bone_name] = anim_data[bone_name].values;
+				}
+
+				if (false) {
+					for (let i = 0; i < anim_data.length; i++) {
+						animation_data[anim_data[i]["name"]] =
+							anim_data[i]["values"];
+
+						console.log(anim_data[i]["values"].length);
+
+						if (anim_data[i]["values"].length > max_anim_step) {
+							max_anim_step = anim_data[i]["values"].length;
+						}
 					}
 				}
-			});
 
-			console.log(bones);
-
-			// animation_data = {};
-
-			max_anim_step = anim_data["Hips"].values.length;
-
-			// animation_data = anim_data;
-			// iterate over anim_data object
-
-			animation_data = {};
-
-			for (let bone_name in anim_data) {
-				animation_data[bone_name] = anim_data[bone_name].values;
-			}
-
-			if (false) {
-				for (let i = 0; i < anim_data.length; i++) {
-					animation_data[anim_data[i]["name"]] =
-						anim_data[i]["values"];
-
-					console.log(anim_data[i]["values"].length);
-
-					if (anim_data[i]["values"].length > max_anim_step) {
-						max_anim_step = anim_data[i]["values"].length;
-					}
-				}
-			}
-
-			console.log(max_anim_step, animation_data);
-		});
+				console.log(max_anim_step, animation_data);
+			},
+		);
 
 		animate();
 	});
