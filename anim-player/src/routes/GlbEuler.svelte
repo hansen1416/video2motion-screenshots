bf98743432c3f2149b785e2ce418f53d62e9ae06
hansen1416 <badapplesweetie@gmail.com>
2024-03-10 10:58:04 +0800

    1

--- anim-player/src/routes/GlbEuler.svelte
+++ anim-player/src/routes/GlbEuler.svelte
@@ -60,8 +60,8 @@
 
 		Promise.all([
 			loadGLTF(`/glb/dors.glb`),
-			// loadJSON(`/anim-json-euler/Zombie Walk.json`),
-			loadJSON(`/output.json`),
+			loadJSON(`/anim-json-euler/Zombie Walk.json`),
+			// loadJSON(`/output.json`),
 		]).then(([glb_model, anim_data]) => {
 			glb_model = glb_model.scene.children[0];
 
